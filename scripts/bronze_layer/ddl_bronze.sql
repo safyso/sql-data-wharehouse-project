6184cf088b09e7f8e02444be7edca76d8186f031
safyso <95561700+safyso@users.noreply.github.com>
2026-09-10 18:45:58 +0300

    Create 'raw_superstore' table in bronze layer
    This script creates the 'raw_superstore' table in the 'bronze' schema of the 'Superstore_DW' database to store unmodified data from the staging area.

---
+++ scripts/bronze_layer/ddl_bronze.sql
@@ -0,0 +1,44 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Layer Tables
+===============================================================================
+Script Purpose:
+    This script creates the raw table 'bronze.raw_superstore' inside the 
+    'Superstore_DW' database. This table acts as the raw persistence layer 
+    to store unmodified data coming from the staging area.
+
+Parameters:
+    None. (This stored procedure / DDL script doesn't accept any parameters)
+
+Dependencies:
+    - Requires the 'Superstore_DW' database and 'bronze' schema to exist.
+===============================================================================
+*/
+
+IF NOT EXISTS (SELECT 1 FROM sys.tables t JOIN sys.schemas s ON t.schema_id = s.schema_id WHERE s.name = 'bronze' AND t.name = 'raw_superstore')
+BEGIN
+CREATE TABLE bronze.raw_superstore (
+    bronze_id INT IDENTITY(1,1) PRIMARY KEY,
+    Row_ID NVARCHAR(250),
+    Order_ID NVARCHAR(250),
+    Order_Date NVARCHAR(250),
+    Ship_Date NVARCHAR(250),
+    Ship_Mode NVARCHAR(250),
+    Customer_ID NVARCHAR(250),
+    Customer_Name NVARCHAR(250),
+    Segment NVARCHAR(250),
+    Country NVARCHAR(250),
+    City NVARCHAR(250),
+    State NVARCHAR(250),
+    Postal_Code NVARCHAR(250),
+    Region NVARCHAR(250),
+    Product_ID NVARCHAR(250),
+    Category NVARCHAR(250),
+    Sub_Category NVARCHAR(250),
+    Product_Name NVARCHAR(250),
+    Sales NVARCHAR(250),
+    Quantity NVARCHAR(250),
+    Discount NVARCHAR(250),
+    Profit NVARCHAR(250));
+END
+GO
