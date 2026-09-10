fddac90721f3eca635fe08e785077792637c8d46
safyso <95561700+safyso@users.noreply.github.com>
2026-09-10 17:35:02 +0300

    Create Superstore_DW database and schemas
    This script creates the 'Superstore_DW' database and sets up four schemas: 'staging', 'bronze', 'silver', and 'gold'. It uses 'IF NOT EXISTS' logic to ensure safe execution without data loss.

--- scripts/init.database.sql
+++ scripts/init.database.sql
@@ -1,1 +1,40 @@
+/*
+===============================================================================
+Create Database and Schemas
+===============================================================================
+Script Purpose:
+    This script creates a new database named 'Superstore_DW' after checking 
+    if it already exists. If it does not exist, it is created safely. 
+    Additionally, the script sets up four schemas within the database: 
+    'staging', 'bronze', 'silver', and 'gold'.
+
+NOTE:
+    This script uses 'IF NOT EXISTS' logic. It is safe to run multiple times 
+    without the risk of dropping the database or losing any existing data.
+===============================================================================
+*/
+
+-- Create the Superstore_DW database if it does not exist
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Superstore_DW')
+    CREATE DATABASE Superstore_DW;
+GO
+ select * from sys.databases;
+
+-- Use the Superstore_DW database
+USE Superstore_DW;
+GO
+
+  -- Schemas for the four data warehouse layers
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'staging')
+    EXEC('CREATE SCHEMA staging');
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'bronze')
+    EXEC('CREATE SCHEMA bronze');
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'silver')
+    EXEC('CREATE SCHEMA silver');
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'gold')
+    EXEC('CREATE SCHEMA gold');
+GO
 
